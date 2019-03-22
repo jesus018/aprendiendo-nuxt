c0a43b8ee51b5c08655e890633d1de108689f0f0
Jesus Agreda <jesus.agreda018@outlook.com>
2019-03-21 20:26:49 -0500

    Iniciando el proyectoA

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div>
+    <h1>Hola {{ msg }}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+      <tr>
+        <th>Nombre</th>
+      </tr>
+
+      <tr v-for="(mun, index) in municipio" :key="index">
+        <td>{{mun}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Jesus Agreda",
+      municipio: ["Mocoa", "VillaGarzon", "Pto Asis"]
+    };
+  },
+  methods: {
+    guardar(){
+      this.municipio.push(this.msg)
+      this.msg = ''
+    }
+  }
+};
+</script>
+
+<style>
+</style>
