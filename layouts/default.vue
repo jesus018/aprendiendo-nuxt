f7ca3b3cc6351cf2825cfb1164682f3fb6309fd6
Jesus Agreda <jesus.agreda018@outlook.com>
2019-03-27 22:38:52 -0500

    Aprendiendo de componentes y layouts

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <navbar></navbar>
+        <Nuxt />
+        esto es un footer
+    </div>
+</template>
+
+<script>
+import navbar from '../components/navbar'
+export default {
+    components: {navbar},
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
   <div>
     <h1>Hola {{ msg }}</h1>
     <input type="text" v-model="msg">
-    <button @click="guardar">Guardar</button>
+    <button @click="guardar" class="btn btn-primary">Guardar</button>
+    <b-button variant='primary'>Guardar</b-button> <!-- Boton hecho mediante componentes-->
     <table>
       <tr>
         <th>Nombre</th>
@@ -17,7 +18,12 @@
 </template>
 
 <script>
+
+import navbar from '../components/navbar'
+//import contacto from './contacto'
+
 export default {
+  components: {navbar},
   data() {
     return {
       msg: "Jesus Agreda",
